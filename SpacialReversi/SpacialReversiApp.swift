a394f22c60f78d12dd92ee7be5cc65b90e2d5857
aaaa <a@a>
2024-01-07 18:39:11 +0900

    Initial Commit

---
+++ SpacialReversi/SpacialReversiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpacialReversiApp.swift
+//  SpacialReversi
+//
+//  Created by FULLNAME on 2024/01/07.
+//
+
+import SwiftUI
+
+@main
+struct SpacialReversiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
