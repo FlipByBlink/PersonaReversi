f29276d449886eddd234df13f129fa3483d9bd5e
aaaa <a@a>
2024-04-05 10:27:47 +0900

    a

--- PersonaReversi/ReversiView/BoardView/SquareView.swift
+++ PersonaReversi/ReversiView/BoardView/SquareView.swift
@@ -4,9 +4,8 @@ struct SquareView: View {
     var index: Int
     @EnvironmentObject var model: 🥽AppModel
     var body: some View {
-        Color.clear
-            .contentShape(.rect)
-            .glassBackgroundEffect(in: .rect)
+        Rectangle()
+            .opacity(0.001)
             .hoverEffect(isEnabled: self.model.puttable(self.index))
             .onTapGesture {
                 if self.model.puttable(self.index) {
